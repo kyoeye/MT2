37eb09c9c25494832dd78d4557b16e3d8a4654e8
萌豚2 <ghhhgvchbvc55667@outlook.com>
2016-10-22 18:46:05 +0800

    提交一些代码

---
+++ MT2/CS/GetXml.cs
@@ -0,0 +1,69 @@
+﻿using MT2.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT2.CS
+{
+    class GetXml
+    {
+        
+        public static async Task <string> GetWebString (string url,string formData)
+        {
+            string RequestUri =  url ;
+            
+            //string requestUri = "";
+            //if (formData.Contains("="))
+            //{
+            //    requestUri = string.Concat(new object[]
+            //    {
+            //        url,
+            //        "?",
+            //        formData,
+            //        "r=",
+            //        DateTime.Now.Ticks
+            //    });
+            //}
+            //else
+            //{
+            //    if (string.IsNullOrEmpty(formData))
+            //    {
+            //        requestUri = url + "?r=" + DateTime.Now.Ticks;
+            //    }
+            //    else
+            //    {
+            //        requestUri = url + formData;
+            //    }
+            //}
+
+            string result;
+
+            try
+            {
+                using (HttpClient httpclient = new HttpClient())
+                {
+                    using (HttpResponseMessage httpResponseMessage = await httpclient.GetAsync(new Uri(RequestUri))) //使用httpResponMessage接收uri返回的信息
+                    {
+                        httpResponseMessage.EnsureSuccessStatusCode(); 
+                        var text = await httpResponseMessage.Content.ReadAsStringAsync(); //异步读取字符串
+                        result = text;
+                    }
+                }
+            }
+            catch(Exception ec)
+            {
+                //后面加个弹窗(～o￣3￣)～
+                var ectc =  ec.ToString();
+                TextDialog ectctc = new TextDialog();
+                ectctc.TopTextblocktext("异常酱");
+                ectctc.Contenttextblocktext(ectc );
+                result = null;
+
+            }
+            return result;
+        } 
+    }
+}
---
+++ MT2/CS/XmlHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MT2.CS
+{
+    class XmlHelper
+    {
+        string[] id = new string[30];
+        string[] uri = new string[30];
+        //注意，这里暂时还不可行，需要先确定目标，不然这样访问会很乱。。。
+        public async void getacg()
+        {
+            string url = "https://yande.re/post.xml?limit=30";
+          
+            var mystring = await GetXml.GetWebString(url, null);
+
+
+            XElement root = XElement.Parse(mystring);
+            IEnumerable<XElement> elements = root.Elements();
+            foreach (var element in elements)
+            {
+                if (element.Name == "post")
+                {
+                    IEnumerable<XAttribute> Items = element.Attributes();
+                    foreach (var item in Items)
+                    {
+                        if (item.Name == "id")
+                        {
+
+                            id[0] = (string)item;
+                            textb.Text = id[0];
+
+                        }
+                        else if (item.Name == "file_url")
+                        {
+                            uri[0] = (string)item;
+                            mybutton.Content = uri[0];
+
+
+                        }
+                        else if (item.Name == "rating")
+                        {
+                            bool fc = (item.Value != "e");
+                            if (fc == false)
+                            {
+                                break;
+                            }
+                            break;
+                        }
+
+                    }
+                }
+                break;
+            }
+
+            //Windows.Data.Xml.Dom.XmlDocument doc = new Windows.Data.Xml.Dom.XmlDocument();
+            //doc.LoadXml (mystring);
+
+            //Windows.Data.Xml.Dom.XmlNodeList schoolNodeList = doc.SelectNodes("/posts");
+
+            //if (schoolNodeList != null)
+            //{
+            //    foreach (XmlNode schoolNode in schoolNodeList)
+            //    {
+
+            //    }
+            //}
+
+
+
+
+            //Windows.Data.Xml.Dom.XmlDocument doc = new Windows.Data.Xml.Dom.XmlDocument();
+            //doc.LoadXml(mystring);
+            //Windows.Data.Xml.Dom.XmlNodeList postsNodeList = doc.SelectNodes("/posts");//一级节点
+            //if (postsNodeList != null)
+            //{
+            //    foreach (Windows.Data.Xml.Dom.XmlAttribute yandeNode in postsNodeList) //循环
+            //    {
+
+            //        Windows.Data.Xml.Dom.XmlAttribute gradesNode = yandeNode.NodeName ;
+            //        {
+            //            System.Xml.XmlNodeList gradeNodeList = gradesNode.ChildNodes;
+
+            //            if (gradeNodeList != null)
+            //            {
+            //                foreach (XmlNode gradeNode in gradeNodeList)
+            //                {
+            //                    textb.Text = yandeNode.Attributes["jpeg_url"].Value;
+
+            //                }
+            //            }
+
+            //        }
+
+
+            //    }
+            //}
+            //else
+            //{
+            //    textb.Text = "一个坏消息，无法找到posts";
+            //}
+        }
+
+
+    }
+}
