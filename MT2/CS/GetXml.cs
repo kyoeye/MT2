ef4ed582f57385be5245a0f513ce3c9a4c88eca1
萌豚2 <ghhhgvchbvc55667@outlook.com>
2016-12-15 11:50:20 +0800

    不稳定的提交

--- MT2/CS/GetXml.cs
+++ MT2/CS/GetXml.cs
@@ -8,11 +8,13 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace MT2.CS
 {
     class GetXml
     {
+
         private CancellationTokenSource cts = new CancellationTokenSource();
 
         public static async Task<string> GetWebString(string url, string formData)
@@ -47,24 +49,16 @@ namespace MT2.CS
 
             try
             {
-                
+
                 IProgress<Windows.Web.Http.HttpProgress> progress = new Progress<Windows.Web.Http.HttpProgress>();
-                
-                   HttpClient httpclient = new HttpClient();
-                   
-                       HttpResponseMessage httpResponseMessage = await httpclient.GetAsync(new Uri(RequestUri)); //使用httpResponMessage接收uri返回的信息
-                      //  HttpStatusCode      statuscode =   httpResponseMessage.StatusCode; //这是一个枚举//可能是不必要的
-                        var s = httpResponseMessage.StatusCode.ToString();
-                        switch ( s )
-                {
-                    case "200":
-                       
-                        break;
-                    case "421":
 
-                        break;   
+                HttpClient httpclient = new HttpClient();
 
-                }
+                HttpResponseMessage httpResponseMessage = await httpclient.GetAsync(new Uri(RequestUri)); //使用httpResponMessage接收uri返回的信息
+                                                                                                          //  HttpStatusCode      statuscode =   httpResponseMessage.StatusCode; //这是一个枚举//可能是不必要的
+                var s = httpResponseMessage.StatusCode.ToString();
+                getStatuscode(s);
+
                 httpResponseMessage.EnsureSuccessStatusCode();
                 var text = await httpResponseMessage.Content.ReadAsStringAsync(); //异步读取字符串
                 result = text;
@@ -89,6 +83,60 @@ namespace MT2.CS
 
             }
             return result;
+        }
+
+        private static void getStatuscode(string s)
+        {
+            string dialog = "";
+            switch (s)
+            {
+                case "200":
+
+                    break;
+                case "403":
+                    dialog = s + "：错误，拒绝访问";
+                    showText(dialog);
+                    break;
+                case "404":
+                    dialog = s + "：错误，没有找到";
+                    showText(dialog);
+                    break;
+                case "420":
+                    dialog = s + "：错误，无效记录，记录无法保存";
+                    showText(dialog);
+                    break;
+                case "421":
+                    dialog = s + "：错误，当日访问次数上限";
+                    showText(dialog);
+                    break;
+                case "422":
+                    dialog = s + "：错误，当前资源被锁定无法修改";
+                    showText(dialog);
+                    break;
+                case "423":
+                    dialog = s + "：错误，资源已经存在";
+                    showText(dialog);
+                    break;
+                case "424":
+                    dialog = s + "：错误，无效参数，给定参数无效";
+                    showText(dialog);
+                    break;
+                case "500":
+                    dialog = s + "：错误，服务器内部错误";
+                    showText(dialog);
+                    break;
+                case "503":
+                    dialog = s + "：错误，服务器无法处理当前请求";
+                    showText(dialog);
+                    break;
+
+
+            }
+        }
+
+        public async static void showText(string a)
+        {
+            await new MessageDialog(a).ShowAsync();
         }
     }
 }
